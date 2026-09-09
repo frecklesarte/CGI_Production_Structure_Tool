7e06266b6f8c472b2c1792b9857a5510eaa86632
frecklesarte <davepekas1@protonmail.com>
2026-09-09 11:06:22 -0600

    Stop tracking .venv virtual environment

---
+++ Source/__main__.py
@@ -0,0 +1,2 @@
+# Initializes the __main__ function and calls all the neccesary python scripts for the production_directory_structurizer
+__name__ == '__main__'
---
+++ Source/ui/directory_structure_ui.py
@@ -0,0 +1,48 @@
+# Initializes the "tKinter" Python STL_UI module for creating the User Interface for the "DirectoryStructures" Pipeline Application
+from tkinter import tk
+from tkinter import ttk
+
+
+root = tk.Tk()
+
+# Setting the window properties
+# Window Title
+root.title("DirectoryStructureTool")
+# Window background color
+root.configure(background="grey24")
+# Window Mix, Max Size specification
+root.minsize(600, 400)
+root.maxsize(1200, 1000)
+# Sets the initial x, y size, as well as the x, y scale factor of the window
+root.geometry("800x600+30+20")
+# Boolean operation, Allow or Not Allow, scaling of x or y axis of 'root' window. Default x=False, y=False.
+root.resizable(False, False)
+
+# Setting the Labels
+# Assigns label objects to variables(ex.label1 = tk.label()).. then calls .pack() on the corresponding variable.
+style = ttk.Style()
+style.configure("BW.TLabel", foreground="black", background="white")
+
+productionDirectorylabel = ttk.Label(root, text="Enter, name of CGI Production", style="BW.TLabel")
+productionDirectorylabel.pack()
+
+
+browseDirectorylabel = ttk.Label(root, text="Browse", style="BW.TLabel")
+browseDirectorylabel.pack()
+
+setDirectorylabel = ttk.Label(root, text="Specify, the TopLevel directory.", style="BW.TLabel")
+# if 'browseDirectorylabel' is not 'specified' upon execution of 'createProductionStructurelabel'.. print(Root Production directory, 'not=>specified')     
+setDirectorylabel.pack()
+
+
+
+## The final label which attempts to setup the creation of the "Production, Directory Structure." 
+createProductionStructurelabel = ttk.Label(root, text="Create if you must.", style="BW.TLabel")    
+createProductionStructurelabel.pack()
+
+
+
+quitApplicati = ttk.Label(root, text="Do or do not, there is no try.\n - Jedi Master")
+quitApplicationlabel.pack()
+
+root.mainloop()
